5c8d96bc33cf2f4ce9656e87a8cec5a3d8a8c2e5
Kieran Sukachevin <kieran@kierans-mbp-2.lan>
2020-10-10 14:44:34 -0400

    paddle class created

--- game.js
+++ game.js
@@ -1,5 +1,18 @@
-let canvas = document.getElementById("gameScreen");
-let ctx = canvas.getContext("2d");
+import Paddle from 'paddle.js';
 
-ctx.fillStyle = "#f00";
-ctx.fillRect(20, 20, 100, 100);
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext('2d');  // 2d context
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+ctx.clearRect(0, 0, 800, 600);  // clears the whole screen (800x600)
+ 
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw();
+// ctx.fillStyle = "#f00";
+// ctx.fillRect(20, 20, 100, 100);
+
+// ctx.fillStyle = "#00f";
+// ctx.fillRect(240, 200, 50, 50);
